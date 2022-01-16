bf5fb15fcd6cb8e9a24b6c6efe7bf282974f726d
gabrielccsantos42 <gabrielccsantos42@gmail.com>
2022-01-16 13:29:48 -0300

    remove line 5:10 - useState

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,7 +2,6 @@ import {ReactComponent as StarFull} from 'assets/img/star-full.svg';
 import {ReactComponent as StarHalf} from 'assets/img/star-half.svg';
 import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 import './style.css';
-import { useState } from 'react';
 
 type Props = {
   score : number;
